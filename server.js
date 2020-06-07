471067ccaa142e0e2bfdddf687ede11c4a4f36e5
Radoslaw Zaborski <radex171@gmail.com>
2020-06-08 00:44:53 +0200

    create basic server with access for all users to views folder & image file and block access to endpoint /user/

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const path = require('path');
+
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.show = (name) => {
+        res.sendFile(path.join(__dirname, `/views/${name}`));
+    }
+    next();
+});
+
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use('/user', (req, res, next) => {
+
+    res.show('forbidden.html');
+
+});
+
+
+app.get('/', (req, res) => {
+    res.show(`home.html`)
+});
+
+app.get('/home', (req, res) => {
+    res.show('home.html')
+});
+
+app.get('/about', (req, res) => {
+    res.show('about.html')
+});
+
+app.get('/404.jpg', (req, res) => {
+    res.sendFile(path.join(__dirname, '/404.jpg'));
+
+});
+app.use((req, res) => {
+    res.status(404).show('404.html');
+});
+app.listen(8000, () => {
+    console.log('server runing on port 8000');
+});
